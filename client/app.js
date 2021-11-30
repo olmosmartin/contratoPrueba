fa8a78449cf8897e82c913a35034263b7f249187
olmosmartin <tiincho18@gmail.com>
2021-11-30 20:22:09 -0300

    contrato conectado con front

--- client/app.js
+++ client/app.js
@@ -1,1 +1,35 @@
-console.log("Hello World!");
+App={
+
+    web3Provider: "",
+    contracts: {},
+    init : () => {
+        console.log("App initialized");
+        App.loadEthereum();
+        App.loadContracts();
+    },
+    
+    loadEthereum : async () => {
+        if (window.ethereum) {
+            App.web3Provider=window.ethereum;
+            console.log("ethereum existe");
+            await window.ethereum.request({method: "eth_requestAccounts"});
+        } else if (window.web3) {
+            web3 = new web3(window.web3.currentProvider);
+        } else {
+            console.log("Non-Ethereum browser detected. You should consider trying MetaMask!");
+        }
+    },
+    loadContracts : async () => {
+        const res = await fetch("TasksContract.json");
+        const tasksContractJSON = await res.json();
+
+        console.log(tasksContractJSON);
+        App.contracts.tasksContract = TruffleContract(tasksContractJSON)
+
+        App.contracts.tasksContract.setProvider(App.web3Provider);
+
+        App.tasksContract = await App.contracts.tasksContract.deployed();
+    }
+}
+
+App.init();
